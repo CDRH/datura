7090709cc70e621192166491abd38ee76845b2d5
Jessica Dussault <jdussault4@gmail.com>
2017-01-05 11:03:03 -0600

    updates old tests for parser
    should move these over to minitests soon

--- test_old/ruby/fixtures/parser_executable.rb
+++ test_old/ruby/fixtures/parser_executable.rb
@@ -3,4 +3,4 @@ require_relative '../../../scripts/ruby/lib/parser.rb'
 # this file exists so that parser_test can execute
 # it via ruby and pass in commandline parameters
 
-post_to_solr_params
+Parser.post_to_solr_params
--- test_old/ruby/lib_tests/parser_test.rb
+++ test_old/ruby/lib_tests/parser_test.rb
@@ -18,32 +18,32 @@ RSpec.describe "Parser Test:" do
   describe "timify" do
     context "given incomplete date time" do
       it "should return nil for year only" do
-        time = timify("2014")
+        time = Parser.timify("2014")
         expect(time).to be_nil
       end
       it "should return nil for year / month only" do
-        time = timify("2014-01")
+        time = Parser.timify("2014-01")
         expect(time).to be_nil
       end
       it "should return nil for year/month/day and partial time" do
-        time = timify("2014-01-01T16")
+        time = Parser.timify("2014-01-01T16")
         expect(time).to be_nil
       end
       it "should return a time for year/month/day" do
-        time = timify("2014-01-01")
+        time = Parser.timify("2014-01-01")
         expect(time).to eq(Time.new("2014-01-01"))
       end
     end
 
     context "given complete date time" do
       it "should work despite various combos of - T :" do
-        time1 = timify("2014:01:01:15:43")
+        time1 = Parser.timify("2014:01:01:15:43")
         expect(time1).to eq(Time.new("2014", "01", "01", "15", "43"))
-        time2 = timify("2014-01-01T15:43")
+        time2 = Parser.timify("2014-01-01T15:43")
         expect(time2).to eq(Time.new("2014", "01", "01", "15", "43"))
       end
       it "should work with shortened months (1 vs 01)" do
-        time = timify("2014-1-1T15:43")
+        time = Parser.timify("2014-1-1T15:43")
         expect(time).to eq(Time.new("2014", "01", "01", "15", "43"))
       end
     end
