d8761a5e2c8e88d746d6aafbdfc91302fb523ec0
Jessica Dussault <jdussault4@gmail.com>
2017-01-27 12:18:13 -0600

    minor additions

--- scripts/ruby/lib/tei_to_es.rb
+++ scripts/ruby/lib/tei_to_es.rb
@@ -255,7 +255,7 @@ module TeiToEs
 
   def self.person
     # TODO will need some examples of how this will work
-    # and put in the xpaths above, also
+    # and put in the xpaths above, also for attributes, etc
     # should contain name, id, and role
     eles = @xml.xpath(@xpaths["person"])
     return eles.map { |p| { "role" => p["role"], "name" => p.text, "id" => nil } }
