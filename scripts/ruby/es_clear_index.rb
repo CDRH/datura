c6c6a7253da60f2c7a2d259662f558c7954cc6bf
Jessica Dussault <jdussault4@gmail.com>
2017-02-03 12:28:53 -0600

    adds script to delete elasticsearch results
    works across all types on index if required
    field + regex supported, defaults to id

---
+++ scripts/ruby/es_clear_index.rb
@@ -0,0 +1,54 @@
+require "json"
+require "rest-client"
+require_relative "lib/requirer.rb"
+
+this_dir = File.dirname(__FILE__)
+
+# run the parameters through the option parser
+params = Parser.clear_index_params
+options = Options.new(params, "#{this_dir}/../../config", "#{this_dir}/../../projects/#{params['project']}/config").all
+project = options["project"]
+
+# verify that the user is really sure about the index they're about to wipe
+puts "Are you sure that you want to remove entries from"
+puts " #{project}'s #{options['environment']} environment?"
+if project == "all"
+  puts "CAUTION: this will remove entries across ALL PROJECTS"
+end
+puts "y/N"
+confirm = STDIN.gets.chomp
+
+if confirm && (confirm == "y" || confirm == "Y")
+  path = options["es_path"] || "http://localhost:9200"
+  url = ""
+  if project == "all"
+    url = "#{path}/_delete_by_query?pretty"
+  else
+    url = "#{path}/#{project}/_delete_by_query?pretty"
+  end
+  puts url
+
+  data = {}
+  if options["regex"]
+    field = options["field"] || "cdrh-identifier"
+    data = {
+      "query" => {
+        "regexp" => { field => options["regex"] }
+      }
+    }
+  else
+    data = {
+      "query" => { "match_all" => {} }
+    }
+  end
+  begin
+    puts "clearing: #{data.to_json}"
+    res = RestClient.post(url, data.to_json, {:content_type => :json})
+    puts res.body
+  rescue => e
+    puts "error posting to ES: #{e.response}"
+  end
+else
+  puts "okay, come back whenever you like!"
+  exit
+end
