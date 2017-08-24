520577f413031fc49020e95742c4fa0bc334f0be
Jessica Dussault <jdussault4@gmail.com>
2017-08-24 16:46:44 -0500

    fixes solr clear index

--- scripts/ruby/solr_clear_index.rb
+++ scripts/ruby/solr_clear_index.rb
@@ -13,7 +13,7 @@ this_dir = File.dirname(__FILE__)
 
 # run the parameters through the option parser
 params = Parser.clear_index_params
-options = Options.new(params, "#{this_dir}/../../config/config.yml", "#{this_dir}/../../collections/#{params['collection']}/config/config.yml").all
+options = Options.new(params, "#{this_dir}/../../config", "#{this_dir}/../../collections/#{params['collection']}/config").all
 
 # verify that the user is really sure about the index they're about to wipe
 puts "Are you sure that you want to remove entries from"
@@ -21,7 +21,7 @@ puts " #{options['collection']}'s #{options['environment']} environment?"
 puts "y/N"
 confirm = STDIN.gets.chomp
 if confirm && (confirm == "y" || confirm == "Y" || confirm == "Yes" || confirm == "yes")
-  url = "#{options["solr_path"]}#{options["solr_core"]}/update"
+  url = "#{options["solr_path"]}/#{options["solr_core"]}/update"
   # create a new solr object
   solr = SolrPoster.new(url)
   puts "Clearing index at #{solr.url}"
