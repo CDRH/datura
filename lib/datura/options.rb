b0709b605bb999790337ca44e0ce90c46d3aa843
Jessica Dussault <jdussault4@gmail.com>
2018-11-06 12:44:33 -0600

    private.yml should not longer exist in gem, rearranging
    will need to make sure users fill out public and private config
    tried to make comments more clear about contents of gem defaults

--- lib/datura/options.rb
+++ lib/datura/options.rb
@@ -34,11 +34,9 @@ class Datura::Options
 
   def read_all_configs(general, collection)
     @general_config_pub = read_config("#{general}/public.yml")
-    @general_config_priv = read_config("#{general}/private.yml")
     @collection_config_pub = read_config("#{collection}/public.yml")
     @collection_config_priv = read_config("#{collection}/private.yml")
     print_message(@general_config_pub, "base public")
-    print_message(@general_config_priv, "base private")
     print_message(@collection_config_pub, "collection public")
     print_message(@collection_config_priv, "collection private")
   end
