e4d79278b9fcf64202943ae9df8e007753eb17a0
William Dewey <william.dewey@unl.edu>
2021-11-03 14:28:44 -0500

    add EadToEsItems class and associated files

---
+++ lib/datura/to_es/ead_to_es_items/request.rb
@@ -0,0 +1,7 @@
+class EadToEsItems < XmlToEs
+
+  # please refer to generic xml to es request file, request.rb
+  # and override methods specific to TEI transformation here
+  # project specific overrides should go in the COLLECTION's overrides!
+
+end
