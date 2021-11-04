c9de6c1827101c0da4eb4028ef6db01564fd3c25
William Dewey <william.dewey@unl.edu>
2021-11-04 11:10:23 -0500

    add all require fields, including unfilled ones

--- lib/datura/to_es/ead_to_es/fields.rb
+++ lib/datura/to_es/ead_to_es/fields.rb
@@ -15,19 +15,19 @@ class EadToEs < XmlToEs
   #   "https://cdrhapi.unl.edu/doc/#{@id}"
   # end
 
-  # def annotations_text
-  #   # TODO what should default behavior be?
-  # end
+  def annotations_text
+    # TODO what should default behavior be?
+  end
 
-  # def category
-  #   category = get_text(@xpaths["category"])
-  #   return category.length > 0 ? CommonXml.normalize_space(category) : "none"
-  # end
+  def category
+  end
 
   # note this does not sort the creators
   def creator
     creators = get_list(@xpaths["creators"])
-    return creators.map { |creator| { "name" => CommonXml.normalize_space(creator) } }
+    if creators
+      return creators.map { |creator| { "name" => CommonXml.normalize_space(creator) } }
+    end
   end
 
   # returns ; delineated string of alphabetized creators
@@ -43,20 +43,20 @@ class EadToEs < XmlToEs
     @options["collection_desc"] || @options["collection"]
   end
 
-  # def contributor
-  #   contribs = []
-  #   @xpaths["contributors"].each do |xpath|
-  #     eles = @xml.xpath(xpath)
-  #     eles.each do |ele|
-  #       contribs << {
-  #         "id" => ele["id"],
-  #         "name" => CommonXml.normalize_space(ele.text),
-  #         "role" => CommonXml.normalize_space(ele["role"])
-  #       }
-  #     end
-  #   end
-  #   contribs.uniq
-  # end
+  def contributor
+    # contribs = []
+    # @xpaths["contributors"].each do |xpath|
+    #   eles = @xml.xpath(xpath)
+    #   eles.each do |ele|
+    #     contribs << {
+    #       "id" => ele["id"],
+    #       "name" => CommonXml.normalize_space(ele.text),
+    #       "role" => CommonXml.normalize_space(ele["role"])
+    #     }
+    #   end
+    # end
+    # contribs.uniq
+  end
 
   def data_type
     "ead"
@@ -67,9 +67,9 @@ class EadToEs < XmlToEs
     return CommonXml.date_standardize(datestr, before)
   end
 
-  # def date_display
-  #   get_text(@xpaths["date_display"])
-  # end
+  def date_display
+    get_text(@xpaths["date_display"])
+  end
 
   def date_not_after
     date(false)
@@ -88,85 +88,80 @@ class EadToEs < XmlToEs
   end
 
   def format
-    matched_format = nil
-    # iterate through all the formats until the first one matches
-    @xpaths["formats"].each do |type, xpath|
-      text = get_text(xpath)
-      matched_format = type if text && text.length > 0
-    end
-    matched_format
+    get_list(@xpaths["format"])
   end
 
-  # def image_id
-  #   # Note: don't pull full path because will be pulled by IIIF
-  #   images = get_list(@xpaths["image_id"])
-  #   images[0] if images
-  # end
+  def image_id
+    # Note: don't pull full path because will be pulled by IIIF
+    # How to deal with this?
+    images = get_list(@xpaths["image_id"])
+    images[0] if images
+  end
 
-  # def keywords
-  #   get_list(@xpaths["keywords"])
-  # end
+  def keywords
+    get_list(@xpaths["keywords"])
+  end
 
-  # def language
-  #   get_text(@xpaths["language"])
-  # end
+  def language
+    get_text(@xpaths["language"])
+  end
 
-  # def languages
-  #   get_list(@xpaths["languages"])
-  # end
+  def languages
+    get_list(@xpaths["languages"])
+  end
 
   def medium
     # Default behavior is the same as "format" method
     format
   end
 
-  # def person
-  #   # TODO will need some examples of how this will work
-  #   # and put in the xpaths above, also for attributes, etc
-  #   # should contain name, id, and role
-  #   eles = @xml.xpath(@xpaths["person"])
-  #   people = eles.map do |p|
-  #     {
-  #       "id" => "",
-  #       "name" => CommonXml.normalize_space(p.text),
-  #       "role" => CommonXml.normalize_space(p["role"])
-  #     }
-  #   end
-  #   return people
-  # end
+  def person
+    # TODO will need some examples of how this will work
+    # and put in the xpaths above, also for attributes, etc
+    # should contain name, id, and role
+    # eles = @xml.xpath(@xpaths["person"])
+    # people = eles.map do |p|
+    #   {
+    #     "id" => "",
+    #     "name" => CommonXml.normalize_space(p.text),
+    #     "role" => CommonXml.normalize_space(p["role"])
+    #   }
+    # end
+    # return people
+  end
 
-  # def people
-  #   @json["person"].map { |p| CommonXml.normalize_space(p["name"]) }
-  # end
+  def people
+    # @json["person"].map { |p| CommonXml.normalize_space(p["name"]) }
+  end
 
-  # def places
-  #   return get_list(@xpaths["places"])
-  # end
+  def places
+    return get_list(@xpaths["places"])
+  end
 
-  # def publisher
-  #   get_text(@xpaths["publisher"])
-  # end
+  def publisher
+    get_text(@xpaths["publisher"])
+  end
 
-  # def recipient
-  #   eles = @xml.xpath(@xpaths["recipient"])
-  #   people = eles.map do |p|
-  #     {
-  #       "id" => "",
-  #       "name" => CommonXml.normalize_space(p.text),
-  #       "role" => "recipient"
-  #     }
-  #   end
-  #   return people
-  # end
+  def recipient
+    # eles = @xml.xpath(@xpaths["recipient"])
+    # people = eles.map do |p|
+    #   {
+    #     "id" => "",
+    #     "name" => CommonXml.normalize_space(p.text),
+    #     "role" => "recipient"
+    #   }
+    # end
+    # return people
+  end
 
-  # def rights
-  #   # Note: override by collection as needed
-  #   get_text(@xpaths["rights"])
-  # end
+  def rights
+    # Note: override by collection as needed
+    get_text(@xpaths["rights"])
+  end
 
-  # def rights_holder
-  #   get_text(@xpaths["rights_holder"])
-  # end
+  def rights_holder
+    get_text(@xpaths["rights_holder"])
+  end
 
   def rights_uri
     # by default collections have no uri associated with them
@@ -174,27 +169,28 @@ class EadToEs < XmlToEs
     # to return specific string or xpath as required
   end
 
-  # def source
-  #   get_text(@xpaths["source"])
-  # end
+  def source
+    get_text(@xpaths["source"])
+  end
 
-  # def subjects
-  #   get_list(@xpaths["subjects"])
-  # end
+  def subjects
+    get_list(@xpaths["subjects"])
+  end
 
-  # def subcategory
-  #   subcategory = get_text(@xpaths["subcategory"])
-  #   subcategory.length > 0 ? subcategory : "none"
-  # end
+  def subcategory
+    # subcategory = get_text(@xpaths["subcategory"])
+    # subcategory.length > 0 ? subcategory : "none"
+  end
 
   def text
     # handling separate fields in array
     # means no worrying about handling spacing between words
     text = []
-    @xpaths.keys.each do [xpath] 
+    @xpaths.keys.each do |xpath| 
       body = get_text(@xpaths[xpath])
       text << body
     end
+    text
     # TODO: do we need to preserve tags like <i> in text? if so, turn get_text to true
     # text << CommonXml.convert_tags_in_string(body)
     # text += text_additional
@@ -223,26 +219,26 @@ class EadToEs < XmlToEs
     get_text(@xpaths["type"])
   end
 
-  # def topics
-  #   get_list(@xpaths["topic"])
-  # end
+  def topics
+    get_list(@xpaths["topic"])
+  end
 
-  # def uri
-  #   # override per collection
-  #   # should point at the live website view of resource
-  # end
+  def uri
+    # override per collection
+    # should point at the live website view of resource
+  end
 
-  # def uri_data
-  #   base = @options["data_base"]
-  #   subpath = "data/#{@options["collection"]}/source/tei"
-  #   return "#{base}/#{subpath}/#{@id}.xml"
-  # end
+  def uri_data
+    base = @options["data_base"]
+    subpath = "data/#{@options["collection"]}/source/tei"
+    return "#{base}/#{subpath}/#{@id}.xml"
+  end
 
-  # def uri_html
-  #   base = @options["data_base"]
-  #   subpath = "data/#{@options["collection"]}/output/#{@options["environment"]}/html"
-  #   return "#{base}/#{subpath}/#{@id}.html"
-  # end
+  def uri_html
+    base = @options["data_base"]
+    subpath = "data/#{@options["collection"]}/output/#{@options["environment"]}/html"
+    return "#{base}/#{subpath}/#{@id}.html"
+  end
 
   def works
     # TODO need to create a list of items, maybe an array of ids
