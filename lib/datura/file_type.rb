0d3c07ad0216f20dfac5a0ac2d3ce5960bd5ef9f
William Dewey <william.dewey@unl.edu>
2021-11-08 12:55:46 -0600

    add a backtrace to error handling

--- lib/datura/file_type.rb
+++ lib/datura/file_type.rb
@@ -69,6 +69,7 @@ class FileType
           begin
             RestClient.put("#{es.index_url}/_doc/#{id}", doc.to_json, {:content_type => :json } )
           rescue => e
+            # byebug
             error = "Error transforming or posting to ES for #{self.filename(false)}: #{e.response}"
           end
         else
@@ -132,7 +133,6 @@ class FileType
         subdocs = file_xml.xpath(xpath)
         subdocs.each do |subdoc|
           file_transformer = classname.new(subdoc, @options, file_xml, self.filename(false))
-          
           es_req << file_transformer.json
         end
       end
@@ -144,6 +144,7 @@ class FileType
     rescue => e
       puts "something went wrong transforming #{self.filename}"
       puts e
+      puts e.backtrace
       raise e
     end
   end
