344381ccb5fdcb55ba9f5daee2b0324fe5f6d25a
William Dewey <william.dewey@unl.edu>
2021-11-11 11:04:00 -0600

    change xpaths to be less specific to Walt Whitman

--- lib/datura/to_es/ead_to_es/xpaths.rb
+++ lib/datura/to_es/ead_to_es/xpaths.rb
@@ -6,36 +6,21 @@ class EadToEs < XmlToEs
   def xpaths_list
       {
         "abstract" => "/ead/archdesc/did/abstract",
-        # "category" => "/TEI/teiHeader/profileDesc/textClass/keywords[@n='category'][1]/term",
-        # "contributors" => [
-        #   "/TEI/teiHeader/revisionDesc/change/name",
-        #   "/TEI/teiHeader/fileDesc/titleStmt/editor"
-        # ],
         "creators" => ["/ead/archdesc/did/origination/persname", "/ead/eadheader/filedesc/titlestmt/creator"],
         "date" => "/ead/eadheader/filedesc/publicationstmt/date",
         "description" => "/ead/archdesc/scopecontent/p",
-        # a non normalized date taken directly from the source material ("Dec 9", "Winter 1889")
-        # "date_display" => "/TEI/teiHeader/fileDesc/sourceDesc/bibl/date",
         "formats" => "/ead/archdesc/did/physdesc/genreform",
-        # "image_id" => "/TEI/text//pb/@facs",
-        # "keywords" => "/TEI/teiHeader/profileDesc/textClass/keywords[@n='keywords']/term",
-        # note: language is global attribute xml:lang
         "identifier" => "/ead/archdesc/did/unitid",
         "language" => "/ead/eadheader/profiledesc/langusage/language",
-        # "languages" => "//body/div1/@lang",
-        # "person" => "/TEI/teiHeader/profileDesc/textClass/keywords[@n='people']/term",
-        # "places" => "/TEI/teiHeader/profileDesc/textClass/keywords[@n='places']/term",
         "publisher" => "/ead/eadheader/filedesc/publicationstmt/publisher",
-        # "recipient" => "/TEI/teiHeader/profileDesc/particDesc/person[@role='recipient']/persName",
         "repository_contact" => "/ead/archdesc/did/repository/address/*",
         "rights" => "/ead/archdesc/descgrp/accessrestrict/p",
         "rights_holder" => "/ead/archdesc/did/repository/corpname",
         "source" => "/ead/archdesc/descgrp/prefercite/p",
         "subjects" => "/ead/archdesc/controlaccess/*[not(name()='head')]",
-        # "subcategory" => "/TEI/teiHeader/profileDesc/textClass/keywords[@n='subcategory'][1]/term",
         "title" => "/ead/archdesc/did/unittitle",
         "text" => "/ead/eadheader/filedesc/titlestmt/*",
-        "items" => "//*[@level='item']/did/unitid[@type='WWA']"
+        "items" => "//*[@level='item']/did/unitid"
       }.merge(override_xpaths)
     end
   end
--- lib/datura/to_es/ead_to_es_items/xpaths.rb
+++ lib/datura/to_es/ead_to_es_items/xpaths.rb
@@ -6,40 +6,15 @@ class EadToEsItems < EadToEs
   def xpaths_list
       {
         "abstract" => "did/abstract",
-        # "category" => "/TEI/teiHeader/profileDesc/textClass/keywords[@n='category'][1]/term",
-        # "contributors" => [
-        #   "/TEI/teiHeader/revisionDesc/change/name",
-        #   "/TEI/teiHeader/fileDesc/titleStmt/editor"
-        # # ],
-        # "creators" => ["/ead/archdesc/did/origination/persname", "/ead/eadheader/filedesc/titlestmt/creator"],
         "date" => "did/unitdate",
         "description" => "scopecontent/p",
-        # a non normalized date taken directly from the source material ("Dec 9", "Winter 1889")
-        # "date_display" => "/TEI/teiHeader/fileDesc/sourceDesc/bibl/date",
         "extent" => "did/physdesc/extent",
         "format" => "did/physdesc/physfacet",
         "image_url" => "did/dao/@href",
-        # "image_id" => "/TEI/text//pb/@facs",
-        # "keywords" => "/TEI/teiHeader/profileDesc/textClass/keywords[@n='keywords']/term",
-        # note: language is global attribute xml:lang
-        "identifier" => "did/unitid[@type='WWA']",
-        # "language" => "(//body/div1/@lang)[1]",
-        # "languages" => "//body/div1/@lang",
-        # "person" => "/TEI/teiHeader/profileDesc/textClass/keywords[@n='people']/term",
-        # "places" => "/TEI/teiHeader/profileDesc/textClass/keywords[@n='places']/term",
-        # "publisher" => "/TEI/teiHeader/fileDesc/sourceDesc/bibl[1]/publisher[1]",
-        # "recipient" => "/TEI/teiHeader/profileDesc/particDesc/person[@role='recipient']/persName",
+        "identifier" => "did/unitid",
         "repository_id" => "did/unitid[@type='repository']",
-        # "rights" => "/ead/archdesc/descgrp/accessrestrict/p",
-        # "rights_holder" => "/ead/archdesc/descgrp/accessrestrict/p",
-        # "source" => "/ead/archdesc/descgrp/prefercite/p",
-        # "subjects" => ["/ead/archdesc/controlaccess/[everything after head;persname, subject]"],
-        # "subcategory" => "/TEI/teiHeader/profileDesc/textClass/keywords[@n='subcategory'][1]/term",
-        # "text" => "//text",
         "title" => "did/unittitle",
-        # "topic" => "/TEI/teiHeader/profileDesc/textClass/keywords[@n='topic']/term",
         "type" => "did/physdesc/genreform",
-        # "works" => "/TEI/teiHeader/profileDesc/textClass/keywords[@n='works']/term",
       }.merge(override_xpaths)
   end
 end
