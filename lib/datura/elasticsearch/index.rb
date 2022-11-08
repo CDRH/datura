a784c1d1c0a3e4438c9f43638f6ff2942ea0bccf
William Dewey <wkdewey@gmail.com>
2022-11-08 15:42:16 -0600

    add output if nested field is invalid

--- lib/datura/elasticsearch/index.rb
+++ lib/datura/elasticsearch/index.rb
@@ -140,6 +140,7 @@ class Datura::Elasticsearch::Index
               next
             else
               # if one of the nested hashes fails, it is invalid
+              puts "Nested field '#{field}' is invalid"
               return false
             end
           end
