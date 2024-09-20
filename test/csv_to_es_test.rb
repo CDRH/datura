3d59f09fccbcc74722e02b0992116d264f581b3a
wkdewey <william.dewey@unl.edu>
2024-09-20 16:47:37 -0500

    update tests to match new schema

--- test/csv_to_es_test.rb
+++ test/csv_to_es_test.rb
@@ -4,26 +4,28 @@ class CsvToEsTest < Minitest::Test
 
   def setup
     path = File.join($fixture_path, "csv", "testing.csv")
-    csv = CSV.read(path, headers: true)
-
+    csv = CSV.read(path, **{
+      encoding: "utf-8",
+      headers: true,
+    })
+    $options["api_version"] = "2.0"
     @test1 = CsvToEs.new(csv[0], $options, csv)
     @test2 = CsvToEs.new(csv[1], $options, csv)
   end
 
   def test_assemble_json
     json = @test1.assemble_json
-    assert_equal 42, json.length
+    assert_equal 58, json.length
     assert_equal "test.001", json["identifier"]
 
     json = @test2.assemble_json
-    assert_equal 42, json.length
+    assert_equal 58, json.length
     assert_equal "test.002", json["identifier"]
   end
 
   def test_csv_to_es_fields
     json = @test1.assemble_json
     assert_equal "1887-01-01", json["date"]
-
     contributors = [{"name"=>"Jessica Dussault"}, {"name"=>"Greg Tunink"}, {"name"=>"Karin Dalziel"}]
     assert_equal contributors, json["contributor"]
 
