f918b012dfa154016853d2a69559fc82cb913f88
William Dewey <wdewey2@unl.edu>
2025-07-08 17:09:54 -0500

    don't ingest items without id or title

--- lib/datura/file_type.rb
+++ lib/datura/file_type.rb
@@ -60,6 +60,11 @@ class FileType
     end
     if transformed && transformed.length > 0
       transformed.each do |doc|
+        if !doc["identifier"] || !doc["title"]
+          puts "skipping item without id or title"
+          puts "check line " + doc.values.join(" ")
+          next
+        end
         id = doc["identifier"]
         # before a document is posted, we need to make sure that the fields validate against the schema
         if es.valid_document?(doc)
