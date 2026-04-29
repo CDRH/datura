e018533c67dd6e371404f730764d5dffcc894050
nichgray <ngray2@unl.edu>
2026-04-29 15:23:31 -0500

    shorten Saxon error messages

--- lib/datura/file_type.rb
+++ lib/datura/file_type.rb
@@ -209,8 +209,12 @@ class FileType
       out = stdout.read
       err = stderr.read
       if err.length > 0
-        msg = "There was an error transforming #{filename}: #{err}"
-        return { "error" => msg }
+        # Extract meaningful lines from Saxon's stderr for user display
+        key_lines = err.lines.select { |l| l.match?(/^Error|SXXP|XPST|XTTE|XTSE|XSLT|Fatal/) }
+        key_lines = err.lines.first(1) if key_lines.empty?
+        msg = "Error transforming #{filename}: \n " + key_lines.map{ |l| l.strip + "..."}.join("\n ")
+        msg += "\n  (full Saxon output logged)" if err.lines.length > key_lines.length
+        return { "error" => msg, "full_error" => err }
       else
         puts "Successfully transformed #{filename}"
         return { "doc" => out }
