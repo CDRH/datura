c4267beda5e2caaec728f3d978c45aab34f63281
nichgray <ngray2@unl.edu>
2026-06-19 14:39:16 -0500

    rename field overrides example file and create process overrides example file

--- lib/datura/python/field_overrides_example.py
+++ lib/datura/python/field_overrides_example.py
@@ -1,4 +1,4 @@
-#copy this file to omeka_overrides.py in your scripts/python directory. Edit the return values as needed
+#copy this file to field_overrides.py in your scripts/python directory. Edit the return values as needed
 
 from field_definitions import FieldDefinitions
 
